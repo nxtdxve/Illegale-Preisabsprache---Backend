a940cbc20e123adae6a99d10d4a945dafa54c48d
nxtdxve <zettlerd@bzz.ch>
2024-04-03 14:00:49 +0200

    Update app.run() for deployment

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
