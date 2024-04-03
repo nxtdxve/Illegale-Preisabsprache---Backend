02c081cc67a0878e6d52792db2da1245be52c899
nxtdxve <zettlerd@bzz.ch>
2024-04-03 12:52:01 +0200

    Add Flask app initialization and blueprint registration

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from .database.db_connection import init_db
+from config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    init_db(app)
+    
+    from .routes.product_routes import product_bp
+    app.register_blueprint(product_bp)
+    from .routes.retailer_routes import retailer_bp
+    app.register_blueprint(retailer_bp)
+    from .routes.subscription_routes import subscription_bp
+    app.register_blueprint(subscription_bp)
+    from .routes.notification_routes import notification_bp
+    app.register_blueprint(notification_bp)
+    
+    return app
