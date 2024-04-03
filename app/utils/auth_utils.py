c413b864b059317bd50ba1fee82424640457ca60
nxtdxve <zettlerd@bzz.ch>
2024-04-03 13:09:36 +0200

    Add require_api_key function to auth_utils.py

---
+++ app/utils/auth_utils.py
@@ -0,0 +1,15 @@
+from flask import request, jsonify, current_app
+from functools import wraps
+
+def require_api_key(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        api_key = current_app.config['API_KEY']
+
+        request_api_key = request.headers.get('X-API-KEY')
+        
+        if request_api_key and request_api_key == api_key:
+            return f(*args, **kwargs)
+        else:
+            return jsonify({"error": "Unauthorized"}), 401
+    return decorated_function
