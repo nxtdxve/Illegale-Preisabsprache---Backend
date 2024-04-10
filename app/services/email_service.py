07e5a2f6b8e27e3807ae04b92ce55a4a951fe4d4
nxtdxve <zettlerd@bzz.ch>
2024-04-10 14:16:34 +0200

    Add email service for price updates

---
+++ app/services/email_service.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from flask import current_app
+
+def send_email(emails, subject, message, category="Price Change"):
+    url = "https://bulk.api.mailtrap.io/api/send"
+    headers = {
+        "Authorization": f"Bearer {current_app.config['MAILTRAP_API_KEY']}",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "from": {"email": "idpa@zettler.dev", "name": "IDPA Price Tracker"},
+        "to": [{"email": email} for email in emails],
+        "subject": subject,
+        "text": message,
+        "category": category
+    }
+    
+    # Stelle sicher, dass das Payload als JSON-String formatiert wird
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    
+    # Debug-Ausgabe
+    print("Status Code:", response.status_code)
+    print("Response Body:", response.text)
+
+""" # Beispiel für den Funktionsaufruf
+emails = ["davidzettler@zb1.ch", "ghostjidihd@gmail.com"]
+subject = "Ihr aktueller Preisupdate"
+message = "Hier ist Ihre Nachricht."
+send_email(emails, subject, message) """
