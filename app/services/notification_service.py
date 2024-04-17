7c1135b90d4a6b6bab33206d59a6788a434a5429
nxtdxve <zettlerd@bzz.ch>
2024-04-17 13:35:32 +0200

    added weekly notifications

--- app/services/notification_service.py
+++ app/services/notification_service.py
@@ -1,6 +1,8 @@
+import email
 from .email_service import send_email
 from app.database.db_connection import mongo
 from bson import ObjectId
+from datetime import datetime, timedelta
 
 def send_price_change_notifications(product_id, new_price, retailer_id):
     subscriptions = mongo.db.subscriptions.find({"product_id": ObjectId(product_id)})
@@ -26,6 +28,32 @@ def send_price_change_notifications(product_id, new_price, retailer_id):
     else:
         print("Keine Abonnements mit E-Mail gefunden.")
 
-
 def send_weekly_updates():
-    ...
+    last_week = datetime.now() - timedelta(days=7)
+    subscriptions = mongo.db.subscriptions.find({"product_id": "*"})
+    email_list = [sub['email'] for sub in subscriptions if 'email' in sub]
+
+    # Preisänderungen der letzten Woche
+    price_changes = mongo.db.price_records.find({"timestamp": {"$gte": last_week}})
+
+    if price_changes.count() > 0:
+        message_parts = []
+        # Informationen für jede Preisänderung
+        for change in price_changes:
+            product = mongo.db.products.find_one({"_id": change['product_id']})
+            retailer = mongo.db.retailers.find_one({"_id": change['retailer_id']})
+
+            if product and retailer:
+                # Formatierung der Nachricht
+                message_part = f"Der Preis für {product['name']} bei {retailer['name']} hat sich geändert: CHF {change['price'].to_decimal()}"
+                message_parts.append(message_part)
+
+        message = "Hier sind die Preisänderungen der letzten Woche:\n" + "\n".join(message_parts)
+    else:
+        message = "Keine Preisänderungen in der letzten Woche."
+
+    if email_list:
+        send_email(email_list, "Wöchentliche Preisaktualisierungen", message, "Weekly Update")
+    else:
+        print("Keine Abonnements mit E-Mail gefunden.")
+
